2dbb36f258598b605bffd7d4ba235a272c21a7db
jocnn <ijann_@outlook.com>
2023-08-31 19:10:08 -0300

    v68 se agrego avatar en el appBar

--- lib/presentation/chat/chat_screen.dart
+++ lib/presentation/chat/chat_screen.dart
@@ -6,7 +6,19 @@ class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Mi amor ❤️")),
+      appBar: AppBar(
+        leading: const Padding(
+          padding: EdgeInsets.all(4.0),
+          child: CircleAvatar(
+            backgroundImage: NetworkImage(
+              "https://celebborn.com/celeb_image/amanda-seyfried-30994.jpg",
+              scale: .3,
+            ),
+          ),
+        ),
+        title: const Text("Mi amor ❤️"),
+        centerTitle: false,
+      ),
       body: Center(
         child: FilledButton.tonal(
           onPressed: () {},
