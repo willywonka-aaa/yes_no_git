4051ed6b03d5c1d9ebf9811e35d9db316a0ba64b
jocnn <ijann_@outlook.com>
2023-08-31 11:07:08 -0300

    v67 agregando paleta de colores

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -1,9 +1,22 @@
 import 'package:flutter/material.dart';
 
+const Color _customColor = Color(0xFF53658F);
+
+const List<Color> _colorThemes = [
+  _customColor,
+  Colors.white,
+  Colors.blueAccent,
+  Colors.pink,
+  Colors.green,
+  Colors.yellowAccent,
+  Colors.orange,
+];
+
 class AppTheme {
   ThemeData theme() {
     return ThemeData(
       useMaterial3: true,
+      colorSchemeSeed: _colorThemes[3],
     );
   }
 }
