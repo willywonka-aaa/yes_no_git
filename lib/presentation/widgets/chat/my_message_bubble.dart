fc08c27dec34693012a32d7488170f1b5ca8ece7
jocnn <ijann_@outlook.com>
2023-09-01 12:09:16 -0300

    v70 usando el tema de la aplicación

--- lib/presentation/widgets/chat/my_message_bubble.dart
+++ lib/presentation/widgets/chat/my_message_bubble.dart
@@ -5,17 +5,22 @@ class MyMessageBubble extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
     return Column(
       crossAxisAlignment: CrossAxisAlignment.end,
       children: [
         Container(
           decoration: BoxDecoration(
-            color: Colors.amberAccent,
+            color: colors.primary,
             borderRadius: BorderRadius.circular(20),
           ),
           child: const Padding(
             padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-            child: Text("Occaecat duis dolor tempor"),
+            child: Text(
+              "Occaecat duis dolor tempor",
+              style: TextStyle(color: Colors.white),
+            ),
           ),
         ),
         const SizedBox(height: 10)
