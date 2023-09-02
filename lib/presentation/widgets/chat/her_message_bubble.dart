cffda54f7573aac485b5a30be9401498b4b8ce0e
jocnn <ijann_@outlook.com>
2023-09-02 18:51:25 -0300

    v72 mostrando un cargador mientras se descarga

--- lib/presentation/widgets/chat/her_message_bubble.dart
+++ lib/presentation/widgets/chat/her_message_bubble.dart
@@ -41,6 +41,16 @@ class _ImageBubble extends StatelessWidget {
         width: size.width * 0.5,
         height: 120,
         fit: BoxFit.cover,
+        loadingBuilder: (context, child, loadingProgress) {
+          if (loadingProgress == null) return child;
+
+          return Container(
+            width: size.width * 0.5,
+            height: 120,
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+            child: const Text("Te están enviando una imagen"),
+          );
+        },
       ),
     );
   }
