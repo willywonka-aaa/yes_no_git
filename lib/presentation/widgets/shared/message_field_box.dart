7994c5ba6af0cfeb2da5cc5545225ce39316807e
jocnn <ijann_@outlook.com>
2023-09-04 18:20:37 -0300

    v84 se emite el válor del campo con  ValueChange

--- lib/presentation/widgets/shared/message_field_box.dart
+++ lib/presentation/widgets/shared/message_field_box.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 class MessageFieldBox extends StatelessWidget {
-  const MessageFieldBox({super.key});
+  final ValueChanged<String> onValue;
+
+  const MessageFieldBox({super.key, required this.onValue});
 
   @override
   Widget build(BuildContext context) {
@@ -24,6 +26,7 @@ class MessageFieldBox extends StatelessWidget {
           final textValue = textController.value.text;
           debugPrint(
               "------------------------ Valor de la caja de texto - $textValue");
+          onValue(textValue);
           textController.clear();
           focusNode.requestFocus();
         },
@@ -41,6 +44,7 @@ class MessageFieldBox extends StatelessWidget {
         debugPrint("------------------------ Submitted $value");
         textController.clear();
         focusNode.requestFocus();
+        onValue(value);
       },
     );
   }
