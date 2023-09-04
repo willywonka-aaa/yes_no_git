06f25b9582f5b65d6eee3429aa901dd61e6ff179
jocnn <ijann_@outlook.com>
2023-09-04 11:03:15 -0300

    v80 gestión de estado

---
+++ lib/presentation/providers/chat_provider.dart
@@ -0,0 +1,15 @@
+import 'package:b_yes_no_git/domain/entities/message.dart';
+import 'package:flutter/material.dart';
+
+class ChatProvider extends ChangeNotifier {
+
+  List<Message> message = [
+    Message(text: "Hola amor", fromWho: FromWho.me),
+    Message(text: "Ya regresaste del trabajo?", fromWho: FromWho.me),
+  ];
+
+  Future<void> sendMessage(String text) async {
+    // TODO:
+  }
+
+}
