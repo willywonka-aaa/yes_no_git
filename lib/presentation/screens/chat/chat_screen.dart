7bce8472841f784e78abbc6ecebe6f2307bb6ac7
jocnn <ijann_@outlook.com>
2023-09-04 12:17:24 -0300

    v82 se uso el chatProvider para mostrar mensajes

--- lib/presentation/screens/chat/chat_screen.dart
+++ lib/presentation/screens/chat/chat_screen.dart
@@ -1,7 +1,10 @@
+import 'package:b_yes_no_git/domain/entities/message.dart';
+import 'package:b_yes_no_git/presentation/providers/chat_provider.dart';
 import 'package:b_yes_no_git/presentation/widgets/chat/her_message_bubble.dart';
 import 'package:b_yes_no_git/presentation/widgets/chat/my_message_bubble.dart';
 import 'package:b_yes_no_git/presentation/widgets/shared/message_field_box.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({super.key});
@@ -30,6 +33,8 @@ class ChatScreen extends StatelessWidget {
 class _ChatView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final chatProvider = context.watch<ChatProvider>();
+
     return SafeArea(
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 10),
@@ -37,9 +42,13 @@ class _ChatView extends StatelessWidget {
           children: [
             Expanded(
               child: ListView.builder(
-                itemCount: 100,
+                itemCount: chatProvider.messageList.length,
                 itemBuilder: (context, index) {
-                  return (index % 2 == 0) ? const MyMessageBubble() : const HerMessageBubble();
+                  final message = chatProvider.messageList[index];
+
+                  return (message.fromWho == FromWho.hers)
+                      ? const MyMessageBubble()
+                      : const HerMessageBubble();
                 },
               ),
             ),
