3a1b410d9a0ebf6c0acca57a9386e04488beabec
jocnn <ijann_@outlook.com>
2023-09-04 11:16:29 -0300

    v81 se agrega ChatProvider de forma global

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:b_yes_no_git/config/theme/app_theme.dart';
+import 'package:b_yes_no_git/presentation/providers/chat_provider.dart';
 import 'package:b_yes_no_git/presentation/screens/chat/chat_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MainApp());
@@ -11,11 +13,16 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Yes No App",
-      debugShowCheckedModeBanner: false,
-      theme: AppTheme(selectedColor: 3).theme(),
-      home: const ChatScreen()
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => ChatProvider())
+      ],
+      child: MaterialApp(
+        title: "Yes No App",
+        debugShowCheckedModeBanner: false,
+        theme: AppTheme(selectedColor: 3).theme(),
+        home: const ChatScreen()
+      ),
     );
   }
 }
