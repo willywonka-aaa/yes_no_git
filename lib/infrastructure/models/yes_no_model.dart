85dbeb2701dbf708acf4c1fdd6105506df2df9da
jocnn <ijann_@outlook.com>
2023-09-07 11:20:07 -0300

    v87 se creo el modelo para la respuesta del endpoint

---
+++ lib/infrastructure/models/yes_no_model.dart
@@ -0,0 +1,13 @@
+class YesNoModel {
+  final String answer;
+  final bool forced;
+  final String image;
+
+  YesNoModel({required this.answer, required this.forced, required this.image});
+
+  factory YesNoModel.fromJsonMap(Map<String, dynamic> json) => YesNoModel(
+        answer: json["answer"],
+        forced: json["forced"],
+        image: json["image"],
+      );
+}
