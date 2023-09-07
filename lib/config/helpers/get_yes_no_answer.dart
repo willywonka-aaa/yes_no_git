99b45482282fc10def8441bae3f35966981c5553
jocnn <ijann_@outlook.com>
2023-09-07 11:31:20 -0300

    v87 se mapea el endpoint y se retorna el mensaje

--- lib/config/helpers/get_yes_no_answer.dart
+++ lib/config/helpers/get_yes_no_answer.dart
@@ -1,12 +1,18 @@
 import 'package:b_yes_no_git/domain/entities/message.dart';
+import 'package:b_yes_no_git/infrastructure/models/yes_no_model.dart';
 import 'package:dio/dio.dart';
 
 class GetYesNoAnswer {
   final _dio = Dio();
-  
+
   Future<Message> getAnswer() async {
     final response = await _dio.get("https://yesno.wtf/api");
+    final yesNoModel = YesNoModel.fromJsonMap(response.data);
 
-    throw UnimplementedError();
+    return Message(
+      text: yesNoModel.answer,
+      fromWho: FromWho.hers,
+      imageUrl: yesNoModel.image,
+    );
   }
 }
